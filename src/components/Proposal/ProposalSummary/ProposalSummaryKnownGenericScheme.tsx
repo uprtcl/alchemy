39d5408ce37a244c9610871ffac975bbbfa41d35
tspoff@gmail.com <tspoff@gmail.com>
2019-08-18 22:28:48 +0200

    initial work

--- src/components/Proposal/ProposalSummary/ProposalSummaryKnownGenericScheme.tsx
+++ src/components/Proposal/ProposalSummary/ProposalSummaryKnownGenericScheme.tsx
@@ -27,6 +27,9 @@ export default class ProposalSummary extends React.Component<IProps> {
     if (genericSchemeInfo.specs.name === "DutchX") {
       return <ProposalSummaryDutchX {...this.props} />;
     }
+    else if (genericSchemeInfo.specs.name === "BondingCurve") {
+      return <ProposalSummaryDutchX {...this.props} />;
+    }
     const proposalSummaryClass = classNames({
       [css.detailView]: detailView,
       [css.transactionModal]: transactionModal,
