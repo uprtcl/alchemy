290dd1b84a5e8244877bcc548f33372b43d86504
AugustoL <me@augustol.com>
2020-06-23 17:00:31 -0300

    feat(landing-page): Add support for embedded ifram website for landing page

--- src/components/Dao/DaoLandingPage.tsx
+++ src/components/Dao/DaoLandingPage.tsx
@@ -23,26 +23,35 @@ export default class DaoLandingPage extends React.Component<IProps> {
   public render() {
     const daoState = this.props.daoState;
 
-    return (
-      <div className={css.landingPage}>
-
-        <div className={css.infoContainer}>
-          <div className={css.titleContainer}>
-            <div className={css.row}>
-              <div className={css.headerText}>{daoConfig.daoName} Voting Dapp</div>
-            </div>
-          </div>
-
-          <div className={css.welcome}>Welcome to {daoConfig.daoName} voting dapp, a decentralized organization built focused on DEFI.</div>
-          
-          <div className={css.visitProposals}>Visit the <Link to={`/dao/${daoState.id}/schemes`}>Proposals section</Link> vote or make a new proposal.</div>
-          
-          <div className={css.visitProposals}>Visit the <Link to={`/dao/${daoState.id}/history`}>History section</Link> To see past proposals information and result.</div>
-          
-          <div className={css.visitProposals}>Visit the <Link to={`/dao/${daoState.id}/members`}>Members section</Link> To see all {daoConfig.daoName} members.</div>
+    if (process.env.DAO_LANDING_PAGE_URL && process.env.DAO_LANDING_PAGE_URL.length > 0) {
+      return(
+        <div className={css.landingPage}>
+          <iframe src="https://gateway.pinata.cloud/ipfs/QmPhoeL14E5SBFBaC4bA3nuRpg3MpxdWVYdPrdXHdQ3EHY/#/">
+          </iframe>
         </div>
-        
-      </div>
-    );
+      )
+    } else {
+      return (
+        <div className={css.landingPage}>
+
+          <div className={css.infoContainer}>
+            <div className={css.titleContainer}>
+              <div className={css.row}>
+                <div className={css.headerText}>{daoConfig.daoName} Voting Dapp</div>
+              </div>
+            </div>
+
+            <div className={css.welcome}>Welcome to {daoConfig.daoName} voting dapp, a decentralized organization built focused on DEFI.</div>
+            
+            <div className={css.visitProposals}>Visit the <Link to={`/dao/${daoState.id}/schemes`}>Proposals section</Link> vote or make a new proposal.</div>
+            
+            <div className={css.visitProposals}>Visit the <Link to={`/dao/${daoState.id}/history`}>History section</Link> To see past proposals information and result.</div>
+            
+            <div className={css.visitProposals}>Visit the <Link to={`/dao/${daoState.id}/members`}>Members section</Link> To see all {daoConfig.daoName} members.</div>
+          </div>
+          
+        </div>
+      );
+    }
   }
 }
